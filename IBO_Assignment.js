5db9903f2a5847c1327a7a95d2b1c8195360ff6e
rahulr41180 <www.rahulr41180@gmail.com>
2022-07-02 21:26:42 +0530

    Implement getUniqueProductCount function and  getUniqueProducts function

--- IBO_Assignment.js
+++ IBO_Assignment.js
@@ -3,21 +3,17 @@
 
 You have been given a list of products which is having property productName, quantity and description.
 
-
 PROBLEM STATEMENTS:
 
 1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
 
 Sample Output for the given listOfProducts will be :
 
-
 {
   "TV": 2,
   "AC": 2,
   "FAN": 1
 }
-
-
 
 2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
 
@@ -94,10 +90,10 @@ const getUniquePrducts = (items) => {
   	let flag = false;
   	for(let j = 0; j<productObj.length; j++) {
     	if(items[i].productName === productObj[j].productName) {
-      	productObj[j]["quantity"] += items[i].quantity;
-        flag = true;
-        break;
-      }
+      	    productObj[j]["quantity"] += items[i].quantity;
+            flag = true;
+            break;
+        }
     }
     if(flag === false) {
     	productObj.push(items[i]);
